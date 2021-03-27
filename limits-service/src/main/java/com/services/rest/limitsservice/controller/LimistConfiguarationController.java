7ce3ee005d702acbe6f0545868bd6d91e26896bb
naoufal_afailal <naoufal.afailal@everis.com>
2021-03-27 13:19:24 +0100

    add class configuration to limits service

--- limits-service/src/main/java/com/services/rest/limitsservice/controller/LimistConfiguarationController.java
+++ limits-service/src/main/java/com/services/rest/limitsservice/controller/LimistConfiguarationController.java
@@ -1,14 +1,18 @@
 package com.services.rest.limitsservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.services.rest.limitsservice.Configuration;
 import com.services.rest.limitsservice.beans.LimitConfiguration;
 
 @RestController
 public class LimistConfiguarationController {
+	@Autowired
+	private Configuration conf;
 	@GetMapping("/limits")
 	public LimitConfiguration retriveLimitsFromConfiguaration() {
-		return new LimitConfiguration(199, 1);
+		return new LimitConfiguration(conf.getMaximum(), conf.getMinimum());
 	}
 }
